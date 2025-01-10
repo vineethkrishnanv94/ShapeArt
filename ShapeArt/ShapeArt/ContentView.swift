eb0be91785b94357cb1beaf9f6c27170c115c7ca
Vineeth Krishnan V <vineethkrishnanv94@gmail.com>
2025-01-10 18:02:15 -0500

    Initial commit made

---
+++ ShapeArt/ShapeArt/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ShapeArt
+//
+//  Created by Vineeth Krishnan V on 10/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShapeArt/ShapeArt/ShapeArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeArtApp.swift
+//  ShapeArt
+//
+//  Created by Vineeth Krishnan V on 10/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ShapeArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
